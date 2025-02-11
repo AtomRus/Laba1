f5cf687bad8bb2ea125c69c45583ad47c8e06a21
AtomRus <mr.vano2006@bk.ru>
2025-02-11 17:57:12 +0400

    Start day 3

--- obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589760e8618f43c40f29138d368fd49a9674c885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b59c63933edfd1436fed848493721bfaa7f6fb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
